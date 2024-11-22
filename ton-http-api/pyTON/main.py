c8fca33b3f6bcf523e0a5346095ed188112a6eb6
ice-hermes <83634377+ice-hermes@users.noreply.github.com>
2024-11-22 15:34:56 +0200

    add correct paht for docs

--- ton-http-api/pyTON/main.py
+++ ton-http-api/pyTON/main.py
@@ -108,7 +108,7 @@ app = FastAPI(
     title="ION HTTP API",
     description=description,
     version=pkg_version,
-    docs_url='/http/v2',
+    docs_url='/http/v2/',
     responses={
         422: {'description': 'Validation Error'},
         504: {'description': 'Lite Server Timeout'}
@@ -278,7 +278,7 @@ def json_rpc(method):
 async def debug_root(request: Request):
     return {"root_path": request.scope.get("root_path")}
 
-@app.get('/healthcheck', include_in_schema=False)
+@app.get('/http/v2/healthcheck', include_in_schema=False)
 async def healthcheck():
     return 'OK'
 
